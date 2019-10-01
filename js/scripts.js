a6a2599d6c1cee75661eb2dd0b4cfe6e404258ba
Amy <pajakamy@yahoo.co.uk>
2019-10-01 22:11:50 +0100

    improved nav menu style

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,8 @@
 /*!
-    Title: Dev Portfolio Template
-    Version: 1.2.1
-    Last Change: 08/27/2017
-    Author: Ryan Fitzgerald
-    Repo: https://github.com/RyanFitzgerald/devportfolio-template
-    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
-
     Description: This file contains all the scripts associated with the single-page
     portfolio website.
 */
-// $('document').ready(function(){
-//     var typed = new Typed('#typed',{
-//         stringsElement: '#typed-strings'
-//     })
-// });
+
 
 (function($) {
 
